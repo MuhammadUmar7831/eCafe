6532301a6db525aa825c57f597befc9ccebdd47b
Muhammad Umar <mu8494759@gmail.com>
2024-05-11 22:19:37 +0500

    state pattern deleted

--- State Pattern/src/State.java
+++
@@ -1,9 +0,0 @@
-public abstract class State {
-    protected Phone phone;
-    public State (Phone phone){
-        this.phone = phone;
-    }
-
-    public abstract String onHome();
-    public abstract String onOffOn();
-}
