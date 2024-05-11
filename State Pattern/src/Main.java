90d6dc18bbc26247dc6d7fa32aa17e34816b256a
Muhammad Umar <mu8494759@gmail.com>
2024-05-11 22:15:44 +0500

    schema added

---
+++ State Pattern/src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Phone phone = new Phone();
+        simulatePhoneClicks(phone);
+    }
+    private static void simulatePhoneClicks(Phone phone) {
+        System.out.println(phone.clickPower());
+        System.out.println(phone.clickPower());
+        System.out.println(phone.clickHome());
+        System.out.println(phone.clickHome());
+        System.out.println(phone.clickHome());
+        System.out.println(phone.clickPower());
+        System.out.println(phone.clickPower());
+        System.out.println(phone.clickHome());
+    }
+}
